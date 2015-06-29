9ad6a36aa3815c041ab1de61a4b91a48445b7402
cigreja <cigreja@live.com>
2015-06-29 15:09:11 -0400

    - new archive Analyster_0.6.5a released

--- src/main/java/com/elle/analyster/Analyster.java
+++ src/main/java/com/elle/analyster/Analyster.java
@@ -752,8 +752,8 @@ public class Analyster extends JFrame implements ITableNameConstants, IColumnCon
     private void jMenuItemFileVersionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemFileVersionActionPerformed
 
         JOptionPane.showMessageDialog(this, "Creation Date: "
-                + "2015-06-26" + "\n"
-                + "Version: " + "0.6.5");
+                + "2015-06-29" + "\n"
+                + "Version: " + "0.6.5a");
     }//GEN-LAST:event_jMenuItemFileVersionActionPerformed
 
     private void textForSearchMouseClicked(MouseEvent evt) {//GEN-FIRST:event_textForSearchMouseClicked
