bdf8c1b2dcd07528f4ab4be78a6b1f735ee414b3
cigreja <cigreja@live.com>
2015-07-06 13:05:05 -0400

    Refactored TableRowFilterSupport class

--- src/main/java/com/elle/analyster/presentation/filter/TableRowFilterSupport.java
+++ src/main/java/com/elle/analyster/presentation/filter/TableRowFilterSupport.java
@@ -8,35 +8,34 @@ import java.beans.PropertyChangeListener;
 import java.util.Collections;
 //This class it was design to assign table data to filter./////
 
-public final class TableRowFilterSupport {
-    private IObjectToStringTranslator translator;
-    private final ITableFilter<?> filter;
-    private boolean actionsVisible = true;
-    private int filterIconPlacement = SwingConstants.LEADING;
-    private boolean useTableRenderers = false;
+public class TableRowFilterSupport {
+    
+    // class attributes
+    private ITableFilter<?> filter;
+    private boolean actionsVisible;
+    private int filterIconPlacement;
+    private boolean useTableRenderers;
 
-    private TableRowFilterSupport( ITableFilter<?> filter ) {
-        if ( filter == null ) throw new NullPointerException();
-        this.filter = filter;
-    }
-
-    public static TableRowFilterSupport forTable( JTable table ) {
-        return new TableRowFilterSupport(new JTableFilter(table));
-    }
-
-    public static TableRowFilterSupport forFilter( ITableFilter<?> filter ) {
-        return new TableRowFilterSupport(filter);
-    }
 
     /**
-     * Additional actions visible in column filter list
-     * @param visible
-     * @return
+     * CONSTRUCTOR
+     * @param table 
      */
-    public TableRowFilterSupport actions( boolean visible ) {
-        this.actionsVisible = visible;
-        return this;
+    public TableRowFilterSupport( JTable table ) {
+        
+        // initialize class variables
+        filter = new JTableFilter(table);
+        actionsVisible = true; // this should start at false and turned on
+        filterIconPlacement = SwingConstants.LEADING;
+        useTableRenderers = false;
     }
+    
+    /************************************************************************
+     ************************ Setters ***************************************
+     ************************************************************************/
+    
+    public void setFilter(ITableFilter<?> filter){this.filter = filter;}
+    public void setActionsVisible( boolean visible ) {actionsVisible = visible;}
 
     /**
      * Set the placement of the filter icon with respect to the existing icon
@@ -44,9 +43,8 @@ public final class TableRowFilterSupport {
      *
      * @param filterIconPlacement either SwingConstants.LEADING or
      *         SwingConstants.TRAILING.
-     * @return this
      */
-    public TableRowFilterSupport filterIconPlacement(int filterIconPlacement) {
+    public void filterIconPlacement(int filterIconPlacement) {
         if (filterIconPlacement != SwingConstants.LEADING &&
                 filterIconPlacement != SwingConstants.TRAILING) {
             throw new UnsupportedOperationException("The filter icon " +         
@@ -54,37 +52,32 @@ public final class TableRowFilterSupport {
                     "SwingConstants.LEADING or SwingConstants.TRAILING");
         }
         this.filterIconPlacement = filterIconPlacement;
-        return this;
     }
 
+    public void useTableRenderers( boolean value ) { useTableRenderers = value;}
 
-    public TableRowFilterSupport searchTransalator( IObjectToStringTranslator translator ) {
-        this.translator = translator;
-        return this;
-    }
+    /************************************************************************
+     ************************ Getters ***************************************
+     ************************************************************************/
+    
+    public ITableFilter<?> getFilter(){return filter;}
+    public boolean getActionsVisible(){return actionsVisible;}
+    public int getFilterIconPlacement(){return filterIconPlacement;}
+    public boolean getUseTableRenderers(){return useTableRenderers;}
+            
+    /************************************************************************
+     ************************ Methods ***************************************
+     ************************************************************************/        
+            
+    public void apply() {
 
-    public TableRowFilterSupport useTableRenderers( boolean value ) {
-        this.useTableRenderers = value;
-        return this;
-    }
-
-    public ITableFilter<?> apply() {
-
-        final TableFilterColumnPopup filterPopup = new TableFilterColumnPopup(filter);
+        // this should not be called and set here
+        TableFilterColumnPopup filterPopup = new TableFilterColumnPopup(filter);
         filterPopup.setEnabled(true);
         filterPopup.setActionsVisible(actionsVisible);
-        filterPopup.setSearchTranslator(translator);
         filterPopup.setUseTableRenderers( useTableRenderers );
 
-        setupTableHeader();
-        
-        return filter;
-    }
-
-
-    private void setupTableHeader() {
-
-        final JTable table = filter.getTable();
+        JTable table = filter.getTable();
         
 
         filter.addChangeListener(new IFilterChangeListener() {
@@ -97,38 +90,20 @@ public final class TableRowFilterSupport {
         });
         
 
-        // make sure that search component is reset after table model changes
-        setupHeaderRenderers(table.getModel(), true );
-
-    }
-
-    private void setupHeaderRenderers( TableModel newModel, boolean fullSetup ) {
-
-        JTable table =  filter.getTable();
-        
-
         FilterTableHeaderRenderer headerRenderer =
                 new FilterTableHeaderRenderer(filter, filterIconPlacement);
-        filter.modelChanged( newModel );
+        
+        filter.modelChanged( table.getModel() ); 
 
         for( TableColumn c:  Collections.list( table.getColumnModel().getColumns()) ) {
             c.setHeaderRenderer( headerRenderer );
         }
 
-        if ( !fullSetup ) return;
-
         table.addPropertyChangeListener("model", new PropertyChangeListener() {
-
             @Override
             public void propertyChange(PropertyChangeEvent e) {
-                setupHeaderRenderers( (TableModel) e.getNewValue(), false );
-            }
-            
-        });
-
-        
-
-    }
-
-
+                filter.modelChanged( (TableModel) e.getNewValue() ); 
+            } 
+        }); // end addPropertyChangeListener
+    } // end apply
 }
